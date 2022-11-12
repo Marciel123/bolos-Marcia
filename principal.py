0ddbb95ff161e74d3d1d91841fd1c2fe11256b1d
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-12 09:35:23 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,7 +2,7 @@
 
 st.title('Aplicativo para compra de bolos da Marcia')
 st.image('bolo cenoura.PNG')
-SepalLengthCm = st.number_input('bolo cenoura')
+
 #st.image('Marciel.PNG')#
 SepalWidthCm = st.number_input('bolo de limão')
 #st.image('Marciel.PNG')#
