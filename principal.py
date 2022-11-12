a30650c2c8e93f8e0695774d4d1ebb7c62d13f6f
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-12 09:25:25 -0300

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+
+dados = pd.read_csv('Iris.csv')
+
+classes = dados['Species']
+nomesColunas = dados.columns.to_list()
+tamanho = len(nomesColunas)
+nomesColunas = nomesColunas[1:tamanho-1]
+features = dados[nomesColunas]
+
+from sklearn.model_selection import train_test_split
+
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
+                                                                               classes,
+                                                                               test_size=0.26,
+                                                                               random_state=3)
+
+
+
+model= GaussianNB()
+
+
+model.fit(features_treino,classes_treino)
+predicoes = model.predict(features_teste)
+
+
+st.title('Aplicativo de IA Marciel')
+st.image('Marciel.PNG')
+SepalLengthCm = st.number_input('Digite o comprimento do caule')
+SepalWidthCm = st.number_input('Digite a largura do caule')
+PetalLengthCm = st.number_input('Digite o comprimento da petala')
+PetalWidthCm = st.number_input('Digite a largura da petala')
+if st.button('Clique aqui'):
+  resultado = model.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
+
+  if resultado == ('Iris-setosa'):
+    st.write('setosa')
+    st.image('Iris_setosa.PNG')
+    
+  if resultado == ('Iris-versicolor'):
+    st.write('versicolor')
+    st.image('Iris_ versicolor.PNG')
+   
+  if resultado == ('Iris-virginica'):
+    st.write('virginica')
+    st.image('Iris_virginica.PNG')
