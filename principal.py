bdabe482f6825fac39400bb5b4feb5b2147746a4
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-12 09:42:48 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,11 +1,39 @@
+import streamlit as st
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+
+dados = pd.read_csv('bolos')
+
+classes = dados['tipos']
+nomesColunas = dados.columns.to_list()
+tamanho = len(nomesColunas)
+nomesColunas = nomesColunas[1:tamanho-1]
+features = dados[nomesColunas]
+
+from sklearn.model_selection import train_test_split
+
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
+                                                                               classes,
+                                                                               test_size=0.26,
+                                                                               random_state=3)
+
+
+
+model= GaussianNB()
+
+
+model.fit(features_treino,classes_treino)
+predicoes = model.predict(features_teste)
 
 
 st.title('Aplicativo para compra de bolos da Marcia')
-st.image('bolo cenoura.PNG')
+st.image('bolo Marcia.PNG')
+SepalLengthCm = st.number_input('qual bolo voce quer')
+SepalWidthCm = st.number_input('Digite o tipo de cobertura')
 
-#st.image('Marciel.PNG')#
-SepalWidthCm = st.number_input('bolo de limão')
-#st.image('Marciel.PNG')#
+if st.button('Clique aqui'):
+  resultado = model.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
 
+ 
 
  
